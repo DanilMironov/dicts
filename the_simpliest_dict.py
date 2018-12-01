fe8a0aa71409c02e4e43158e1a7e355b32e25a89
DanilMironov <41448941+DanilMironov@users.noreply.github.com>
2018-12-01 23:33:34 +0500

    Add files via upload

---
+++ the_simpliest_dict.py
@@ -0,0 +1,49 @@
+class LinearArray:
+    def __init__(self):
+        self._data = []
+
+    def add(self, key, value):
+        new_element = (key, value)
+        if key in self.keys():
+            for element in self._data:
+                if element[0] == key:
+                    self._data.remove(element)
+                    self._data.append(new_element)
+        else:
+            self._data.append(new_element)
+
+    def get(self, key):
+        for element in self._data:
+            if key == element[0]:
+                return element[1]
+        return None
+
+    def values(self):
+        result = []
+        for element in self._data:
+            result.append(element[1])
+        return result
+
+    def items(self):
+        return self._data
+
+    def keys(self):
+        result = []
+        for element in self._data:
+            result.append(element[0])
+        return result
+
+    def pop(self, key):
+        for i in range(0, len(self._data)):
+            element = self._data[i]
+            if element[0] == key:
+                result = element[1]
+                self._data.remove(element)
+                return result
+        raise KeyError("This key doesn't exist")
+
+    def clear(self):
+        self._data.clear()
+
+    def copy(self):
+        return self._data.copy()
