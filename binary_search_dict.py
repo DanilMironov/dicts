15d8f9e07d6c3c60cd31352550aa5a13b2ae2b88
DanilMironov <41448941+DanilMironov@users.noreply.github.com>
2018-12-12 22:56:42 +0500

    Add files via upload

---
+++ binary_search_dict.py
@@ -0,0 +1,77 @@
+class BinaryArray:
+    def __init__(self):
+        self._data = []
+
+    def add(self, key, value):
+        new_element = (key, value)
+        if key in self.keys():
+            self._data.sort()
+            left = 0
+            right = len(self._data)
+            while left != right:
+                middle = (left + right) // 2
+                element = self._data[middle]
+                if key == element[0]:
+                    self._data.remove(element)
+                    self._data.append(new_element)
+                elif key < element[0]:
+                    right = middle
+                else:
+                    left = middle + 1
+        else:
+            self._data.append(new_element)
+
+    def get(self, key):
+        self._data.sort()
+        left = 0
+        right = len(self._data)
+        while left != right:
+            middle = (left + right) // 2
+            if key == self._data[middle][0]:
+                return self._data[middle][1]
+            elif key < self._data[middle][0]:
+                right = middle
+            else:
+                left = middle + 1
+        return None
+
+    def values(self):
+        result = []
+        for element in self._data:
+            result.append(element[1])
+        return result
+
+    def items(self):
+        return self._data
+
+    def keys(self):
+        result = []
+        for element in self._data:
+            result.append(element[0])
+        return result
+
+    def pop(self, key):
+        self._data.sort()
+        left = 0
+        right = len(self._data)
+        while left != right:
+            middle = (left + right) // 2
+            element = self._data[middle]
+            if key == element[0]:
+                result = element[1]
+                self._data.remove(element)
+                return result
+            elif key < element[0]:
+                right = middle
+            else:
+                left = middle + 1
+        raise KeyError("This key doesn't exist")
+
+    def clear(self):
+        self._data.clear()
+
+    def copy(self):
+        new_data = self._data.copy()
+        dic = BinaryArray()
+        dic._data = new_data
+        return dic
---
+++ binary_tree_dict.py
@@ -0,0 +1,58 @@
+from collections import deque
+from binary_tree import BinaryTree
+
+
+class TreeDict:
+    def __init__(self):
+        self._tree = BinaryTree()
+
+    def add(self, key, value):
+        self._tree.insert(key, value)
+
+    def get(self, key):
+        return self._tree.find(key)[1]
+
+    def items(self):
+        units = deque()
+        items = []
+        units.append(self._tree.root)
+        while True:
+            try:
+                unit = units.popleft()
+                items.append((unit.key, unit.value))
+                if unit.right is not None:
+                    items.append(unit.right)
+                if unit.left is not None:
+                    items.append(unit.left)
+            except IndexError:
+                return items
+
+    def values(self):
+        units = deque()
+        values = []
+        units.append(self._tree.root.value)
+        while True:
+            try:
+                unit = units.popleft()
+                values.append(unit.value)
+                if unit.right is not None:
+                    units.append(unit.right)
+                if unit.left is not None:
+                    units.append(unit.left)
+            except IndexError:
+                return values
+
+    def keys(self):
+        units = deque()
+        keys = []
+        units.append(self._tree.root.key)
+        while True:
+            try:
+                unit = units.popleft()
+                keys.append(unit.key)
+                if unit.right is not None:
+                    units.append(unit.right)
+                if unit.left is not None:
+                    units.append(unit.left)
+            except IndexError:
+                return keys
