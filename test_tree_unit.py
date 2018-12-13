c4047c5a707daf68d32e52b631dd9625c4d56708
DanilMironov <41448941+DanilMironov@users.noreply.github.com>
2018-12-13 13:45:33 +0500

    Add files via upload

---
+++ test_tree_unit.py
@@ -0,0 +1,15 @@
+import unittest
+from tree_unit import TreeUnit
+
+
+class TestTreeUnit(unittest.TestCase):
+    def test_init(self):
+        unit = TreeUnit('key', 'value')
+        self.assertEqual(unit.key, 'key')
+        self.assertEqual(unit.value, 'value')
+        
+    def test_copy(self):
+        unit = TreeUnit('key', 'value')
+        copied_unit = unit.copy()
+        self.assertEqual(copied_unit.value, 'value')
+        self.assertEqual(copied_unit.key, 'key')
